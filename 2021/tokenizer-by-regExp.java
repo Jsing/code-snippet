62d33abaa78f9a92ed970ce5b16aa68852cb9ca7
Jsing <jsing@contec.kr>
2021-08-24 16:19:55 +0900

    tokenizer-by-regExp.java

---
+++ 2021/tokenizer-by-regExp.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+void tokenizeByRegExp() {
+    String inputText = "A101B4C22";
+    Pattern pattern = Pattern.compile("[A-Z][0-9]+");
+    Matcher matcher = pattern.matcher(inputText);
+
+    while(matcher.find()) {
+        System.out.println(matcher.group());
+    }
+}P
